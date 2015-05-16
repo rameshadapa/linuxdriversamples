1b531f7fd207a01366e27645ac3b225e54e65431
Ramesh Adapa <ramesh.adapa86@gmail.com>
2015-05-16 21:33:19 +0530

    Init hello module.

---
+++ hello/hello.c
@@ -0,0 +1,24 @@
+/** hello.c **/
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init hello_init(void)
+{
+ 	printk("Entering into beautiful world.\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	printk("Exiting from the cruel world.\n");
+}
+
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Hello Module");
+MODULE_AUTHOR("Ramesh Adapa (rameshadapa32@gmail.com)");
